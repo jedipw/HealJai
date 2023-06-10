f1b51b6f76b4a94638b52b4c658a13231c1a89db
SaranratR <fahhiha36@gmail.com>
2023-06-11 00:32:34 +0700

    Home page v.3

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -9,12 +9,19 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: Colors.white,
+      backgroundColor: Colors.white,
       appBar: PreferredSize(
-        preferredSize: Size(double.infinity, 80), // Set the desired size
+        preferredSize: const Size(double.infinity, 70), // Set the desired size
         child: AppBar(
           backgroundColor: Colors.white,
           elevation: 0,
+           bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(4), // Set the thickness of the line
+            child: Container(
+              height: 1, // Set the height of the line
+              color: const Color.fromARGB(255, 210, 210, 210), // Set the color of the line
+            ),
+          ),
           title: const Padding(
             padding: EdgeInsets.only(top: 20, left: 5),
             child: Align(
@@ -25,35 +32,38 @@ class HomeView extends StatelessWidget {
                   fontFamily: 'Poppins',
                   fontStyle: FontStyle.normal,
                   fontWeight: FontWeight.w600,
-                  fontSize: 35,
+                  fontSize: 24,
                   color: primaryPurple,
                 ),
               ),
             ),
           ),
           actions: [
-            InkWell(
-        onTap: () {
-          // Add your chat icon onPressed logic here
-          Navigator.of(context).pushNamed(
-            healTalkRoute,
-          );
-        },
-        child: Padding(
-          padding: const EdgeInsets.only(top: 16, right: 16),
-          child: Image.asset(
-            'assets/icons/chat.png', // Replace with the path to your custom image
-            width: 30,
-            height: 30,
-          ),
-        ),
-      ),
+            Container(
+              margin: const EdgeInsets.only(top: 12),
+              child: IconButton(
+                onPressed: () {
+                  // Add your chat icon onPressed logic here
+                  Navigator.of(context).pushNamed(
+                    paymentRoute,
+                  );
+                },
+                icon: Image.asset(
+                  'assets/icons/chat.png', // Replace with the path to your custom image
+                  width: 25,
+                  height: 25,
+                ),
+              ),
+            ),
           ],
         ),
       ),
       body: Column(
         children: [
-         
+          // const Divider(
+          //   height: 2,
+          //   color: Colors.grey,
+          // ),
           Expanded(
             child: ListView.builder(
                 itemCount: 3, // Number of posts
@@ -63,7 +73,7 @@ class HomeView extends StatelessWidget {
                   String name = '';
                   String post = '';
                   String caption = '';
-          
+
                   if (index == 0) {
                     imageUrl =
                         'https://th.bing.com/th/id/R.58f60f6b81ae6d054b6b4910a9771fbf?rik=%2fKjN%2fqPR7wXaUw&pid=ImgRaw&r=0';
@@ -77,7 +87,8 @@ class HomeView extends StatelessWidget {
                         'https://s3.amazonaws.com/freestock-prod/450/freestock_49658134.jpg';
                     name = 'Dr. Pornpimon Kusonbhun';
                     post = 'assets/images/quote2.jpg';
-                    caption = ' Taking care of your mental health is a journey worth investing in. 🌈 #SelfCareMatters.  Practicing self-care is not selfish; it is necessary! Set aside time for activities that bring you joy, relaxation, and rejuvenation. Remember, you can not pour from an empty cup.';
+                    caption =
+                        ' Taking care of your mental health is a journey worth investing in. 🌈 #SelfCareMatters.  Practicing self-care is not selfish; it is necessary! Set aside time for activities that bring you joy, relaxation, and rejuvenation. Remember, you can not pour from an empty cup.';
                   } else if (index == 2) {
                     imageUrl =
                         'https://img.freepik.com/free-photo/smiling-touching-arms-crossed-room-hospital_1134-799.jpg?t=st=1686406430~exp=1686407030~hmac=db7d79cf1f47e3bd475b2d2cffb875388078e256cf52a176894dce8acc079b30';
@@ -86,26 +97,29 @@ class HomeView extends StatelessWidget {
                     caption =
                         ' Mental health is not a destination it is a lifelong journey of growth and resilience. 🌱💪 #EmbraceTheProcess';
                   }
-          
+
                   return Column(
                     children: [
-                      Divider( // Line between each ListView item
-                    height: 4,
-                    color: Colors.grey,
-                  ),
+                      const Divider(
+                        // Line between each ListView item
+                        height: 1,
+                        color: Colors.grey,
+                      ),
                       Container(
                           margin: const EdgeInsets.only(bottom: 16, top: 10),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 16),
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 16),
                                 child: Row(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     Container(
-                                      width: 53,
-                                      height: 56,
+                                      margin: const EdgeInsets.only(left: 4),
+                                      width: 40,
+                                      height: 43,
                                       decoration: BoxDecoration(
                                         shape: BoxShape.circle,
                                         image: DecorationImage(
@@ -116,8 +130,10 @@ class HomeView extends StatelessWidget {
                                     ),
                                     const SizedBox(width: 8),
                                     Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
                                       children: [
                                         const SizedBox(
                                           height: 14,
@@ -125,7 +141,7 @@ class HomeView extends StatelessWidget {
                                         Text(
                                           name,
                                           style: const TextStyle(
-                                              fontSize: 18,
+                                              fontSize: 14,
                                               fontWeight: FontWeight.w500,
                                               fontFamily: 'Poppins'),
                                         ),
@@ -158,7 +174,7 @@ class HomeView extends StatelessWidget {
                                   child: Center(
                                     child: Image.asset(
                                       'assets/icons/dot.png',
-                                      width: 50, 
+                                      width: 50,
                                       height: 50,
                                     ),
                                   ),
@@ -167,12 +183,10 @@ class HomeView extends StatelessWidget {
                               Align(
                                 alignment: Alignment.bottomLeft,
                                 child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 16,
-                                    vertical: 8,
-                                  ),
+                                  padding: const EdgeInsets.fromLTRB(24, 8, 16, 8),
                                   child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
                                     mainAxisSize: MainAxisSize.min,
                                     children: [
                                       RichText(
@@ -207,101 +221,69 @@ class HomeView extends StatelessWidget {
                   );
                 }),
           ),
+          Container(
+            decoration: const BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                    color: Color.fromARGB(255, 210, 210, 210), width: 1),
+              ),
+            ),
+            child: BottomNavigationBar(
+              items: [
+                const BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home_filled,
+                    size: 32,
+                  ),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: InkWell(
+                    onTap: () {},
+                    child: Image.asset(
+                      'assets/icons/noti.png', // Replace with the path to your custom image
+                      width: 32,
+                      height: 32,
+                    ),
+                  ),
+                  label: '',
+                ),
+                const BottomNavigationBarItem(
+                  icon: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.menu_rounded,
+                      size: 32,
+                      color: Color.fromARGB(255, 194, 194, 194),
+                    ),
+                  ),
+                  label: '',
+                ),
+              ],
+              selectedItemColor: primaryPurple,
+              onTap: (index) {
+                if (index == 0) {
+                  // Handle home icon pressed
+                } else if (index == 1) {
+                  Navigator.of(context).pushNamed(
+                    // navigates to paymentRoute screen and removes previous routes
+                    paymentRoute,
+                  );
+                } else if (index == 2) {
+                  logout().then(
+                      (value) => Navigator.of(context).pushNamedAndRemoveUntil(
+                            // navigates to homeRoute screen and removes previous routes
+                            loginRoute,
+                            (route) => false,
+                          ));
+                }
+              },
+            ),
+          )
         ],
       ),
 
       // Replace with your desired body content
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          const BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home_filled,
-              size: 42,
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: InkWell(
-        onTap: () {
-          Navigator.of(context).pushNamed(
-            // navigates to paymentRoute screen and removes previous routes
-            paymentRoute,
-          );
-        },
-        child: Image.asset(
-          'assets/icons/noti.png', // Replace with the path to your custom image
-          width: 42,
-          height: 42,
-        ),
-      ),
-      label: '',
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(
-              Icons.menu_rounded,
-              size: 42,
-              color: Color.fromARGB(255, 194, 194, 194),
-            ),
-            label: '',
-          ),
-        ],
-        selectedItemColor: primaryPurple,
-        onTap: (index) {
-          if (index == 0) {
-            // Handle home icon pressed
-          } else if (index == 1) {
-            Navigator.of(context).pushNamed(
-              // navigates to paymentRoute screen and removes previous routes
-              paymentRoute,
-            );
-          } else if (index == 2) {
-            Navigator.of(context).pushNamedAndRemoveUntil(
-              // navigates to loginRoute screen and removes previous routes
-              loginRoute,
-              (route) => false,
-            );
-          }
-        },
-      ),
     );
   }
 }
-//       body: Row(
-//   mainAxisAlignment: MainAxisAlignment.center,
-//   children: [
-//     Column(
-//       mainAxisAlignment: MainAxisAlignment.center,
-//       children: [
-//         IconButton(
-//           icon: const Icon(Icons.home),
-//           onPressed: () {
-//             // Add your home icon onPressed logic here
-//           },
-//         ),
-//         ElevatedButton(
-//           onPressed: () {
-//             Navigator.of(context).pushNamed(
-//               // navigates to paymentRoute screen and removes previous routes
-//               paymentRoute,
-//             );
-//           },
-//           child: const Icon(Icons.notifications),
-//         ),
-//         ElevatedButton(
-//           onPressed: () {
-//             Navigator.of(context).pushNamedAndRemoveUntil(
-//               // navigates to loginRoute screen and removes previous routes
-//               loginRoute,
-//               (route) => false,
-//             );
-//           },
-//           child: const Text('Logout'),
-//         ),
-//       ],
-//     ),
-//   ],
-// ),
-
-//     );
-//   }
-// }
