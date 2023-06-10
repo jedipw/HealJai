2408cd6afdce05cae5a64590d0f6523fb8332867
Pawin Nakvisai <pawinnak2@gmail.com>
2023-06-10 20:40:59 +0700

    remove unnecessities

--- lib/utilities/heal_talk/heal_talk_psychiatrist_chat.dart
+++ lib/utilities/heal_talk/heal_talk_psychiatrist_chat.dart
@@ -1,6 +1,4 @@
 import 'dart:developer';
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:healjai/constants/color.dart';
