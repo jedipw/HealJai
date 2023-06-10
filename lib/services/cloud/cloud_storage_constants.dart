2170fe9aa86aeb69d30c561e78ca0c49080f35ec
Pawin Nakvisai <pawinnak2@gmail.com>
2023-06-10 19:40:16 +0700

    Chat back-end

--- lib/services/cloud/cloud_storage_constants.dart
+++ lib/services/cloud/cloud_storage_constants.dart
@@ -1,9 +1,18 @@
 const userCollection = 'User';
 const isPsychiatristField = 'isPsychiatrist'; 
 
+
 const chatMessageCollection = 'ChatMessage';
 const isReadField = 'isRead';
 const sendAtField = 'sendAt';
 const senderIdField = 'senderId';
 const textField = 'text';
 const userIdField = 'userId';
+
+const psychiatristProfileCollection = 'PsychiatristProfile';
+const nameField = 'name';
+const namePrefixField = 'namePrefix';
+const pictureURLField = 'pictureURL';
+
+const userTagNumberCollection = 'UserTagNumber';
+const userTagNumberField = 'userTagNumber';
